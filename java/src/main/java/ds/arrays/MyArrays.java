cb73b990645a7dd2744207f01c20c7c0b93cb12f
happy.bhalla <happy.bhalla@redbus.com>
2021-10-01 19:59:55 +0530

    setup practice code

---
+++ java/src/main/java/ds/arrays/MyArrays.java
@@ -0,0 +1,11 @@
+package ds.arrays;
+
+public class MyArrays {
+
+    public static void main(String args[]) {
+
+        System.out.println("My first prog");
+
+    }
+}
+
---
+++ java/src/main/java/ds/arrays/MyArrays2.java
@@ -0,0 +1,7 @@
+package ds.arrays;
+
+public class MyArrays2 {
+    public static void main(String[] args) {
+        System.out.println("My 2nd prog");
+    }
+}
