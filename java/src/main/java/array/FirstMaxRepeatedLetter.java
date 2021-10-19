5fed94d74d6e6c23a11df15b67057049e8dea27d
happy.bhalla <happy.bhalla@redbus.com>
2021-10-19 14:46:42 +0530

    questions

---
+++ java/src/main/java/array/FirstMaxRepeatedLetter.java
@@ -0,0 +1,54 @@
+package array;
+
+import java.util.*;
+
+/*
+For example: "Today, is the greatest day ever!" should return
+ * greatest because it has 2 e's (and 2 t's)
+ */
+public class FirstMaxRepeatedLetter {
+
+    private static String MaxLetter(String str){
+
+        String[] words  = str.split(" ");
+        Map<Integer,String> res = new TreeMap<Integer, String>(Collections.reverseOrder());
+        for(String word : words){
+            Set<Character> letterSet = new HashSet<Character>();
+            Set<Character> letterAdd = new HashSet<Character>();
+            int lcount  = 0;
+            for ( int i = 0 ;i<word.length()-1;i++){
+                System.out.println(word);
+                if (letterSet.contains(word.charAt(i))){
+                    letterAdd.add(word.charAt(i));
+
+                }else {
+                    letterSet.add(word.charAt(i));
+                }
+
+
+            }
+            if(letterAdd.size()>0){
+                if(!res.containsKey(letterAdd.size()) )
+                  res.put(letterAdd.size(),word);
+            }else {
+                continue;
+            }
+
+        }
+
+        for (Map.Entry<Integer,String> entry : res.entrySet()){
+            return entry.getValue();
+
+        }
+
+
+     return "-1";
+    }
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        MaxLetter(line);
+
+    }
+}
---
+++ java/src/main/java/array/StockPrice.java
@@ -0,0 +1,40 @@
+package array;
+
+public class StockPrice {
+
+
+    private int MaxProfit(int [] ret){
+        int maxProfit =0 ;
+        for(int j =0 ; j <ret.length; j++){
+            int buy = ret [j];
+            for(int k =j+1 ; k<ret.length; k++ ){
+                int dayProfit = 0;
+                for(int m = k ; m < ret.length; m++){
+
+                    dayProfit = dayProfit + (ret[m] - ret[m-1]);
+
+                }
+
+                if(maxProfit < dayProfit){
+                    maxProfit = dayProfit;
+                }
+            }
+        }
+        System.out.print(maxProfit);
+        return maxProfit;
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    StockPrice obj = new StockPrice();
+    int input[] = {5030607, 3485715, 2500526, 676233, 7968122, 7455631, 2382510};
+    System.out.println(input.length);
+    obj.MaxProfit(input);
+
+    }
+}
+//552236226
